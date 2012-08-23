3cd9501511b5592396f89316027edfa7f03b3c1a
David Grilli <djgrill@gmail.com>
2012-08-23 23:53:57 +0200

    No more .htaccess file for now
    deleted:    .htaccess
    modified:   index.php

--- index.php
+++ index.php
@@ -1,3 +1,87 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-?>
+<html xmlns="http://www.w3.org/1999/xhtml" itemscope itemtype="http://schema.org/Person">
+
+<head>
+  <title>About - David Grilli</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+  <meta name="name" content="David Grilli" />
+  <meta name="description" content="David Grilli: Software Developer from Ciudad Guayana, Venezuela; currently living in Enschede, Netherlands." />
+  <meta name="keywords" content="david,grilli,geek,fun,software,developer,venezuela,guayana,ucab,enschede,netherlands,computer,engineering,gtug,gamer,videogame,djgrill" />
+  <meta name="robots" content="NOODP,NOYDIR" />
+  <meta itemprop="image" content="https://lh5.googleusercontent.com/-n6ybEefGnKY/TrtAOpcDRQI/AAAAAAAAomI/UgcqHVm8HEU/s416/DGsmile.png" />
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Maven+Pro:regular,bold&amp;v2" />
+  <link rel="icon" type="image/x-ico" href="../img/favicon.ico" />
+  <link rel="shortcut icon" type="image/x-ico" href="../img/favicon.ico" />
+  <script type="text/javascript" src="../js/script.js"></script>
+</head>
+
+<body>
+  <div id="wrapper">
+    <div id="lan">
+      <span id="lanEN" class="selected">en</span>
+      <span>/</span>
+      <span id="lanES">es</span>
+    </div>
+
+    <div id="header">
+      <h1>dav<span class="coolRed">id</span>grill<span class="coolRed">id</span>&iacute;az</h1>
+    </div>
+
+    <div id="nav">
+      <ul>
+        <li><a class="item selected">About</a></li>
+        <li><a href="http://blog.davidgrilli.com/" target="_blank" class="item">Blog</a></li>
+        <li><a href="../contact/" class="item">Contact</a></li>
+        <li><a href="../developer/" class="item">Developer</a></li>
+        <li><a href="../experience/" class="item">Experience</a></li>
+        <li><a href="http://facebook.com/djgrill/" target="_blank" class="item">Facebook</a></li>
+        <li><a href="http://profiles.google.com/djgrill/about/" target="_blank" class="item">Google+</a></li>
+      </ul>
+    </div>
+
+    <div class="lnsep"></div>
+
+    <div id="content">
+      <div id="leftContent">
+        <p><strong>David Grilli D&iacute;az</strong> is a Software Developer from Ciudad Guayana, Venezuela. He's passionate about innovative technologies and loves to learn new things both related and unrelated to IT or Software Development. He's a Computer Engineer from <a href="http://www.ucab.edu.ve/" target="_blank">UCAB</a> and he's coursing the CCNA career certification at Cisco Networking Academy.</p>
+        <br/>
+        <p>He's a Computer Engineer from <a href="http://www.ucab.edu.ve/" target="_blank">UCAB</a> and he's coursing the CCNA career certification at Cisco Networking Academy. He's also the manager of <a href="http://guayana.gtugs.org/" target="_blank">Guayana GTUG</a>, a community for people interested in Google's developer technology.</p>
+        <br/>
+        <p>When David was in High School, he showed a notable interest in Basic Sciences (particularly Math and Physics); but it was not until he began his undergraduate studies when he discovered his passion for Software Development.</p>
+        <br/>
+        <p>He likes running, swimming, watching movies, playing videogames and hanging out with his family and friends.</p>
+      </div>
+
+      <div id="rightContent">
+        <a href="https://twitter.com/djGrill" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @djGrill</a>
+        <script type="text/javascript">!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+        <br/>
+        <br/>
+        <g:plusone size="medium"></g:plusone>
+      </div>
+    </div>
+
+    <div class="lnsep"></div>
+
+    <div id="footer">
+      <span>David Grilli D&iacute;az&nbsp;&copy;&nbsp;2012</span>
+      <table border="0" align="right">
+        <tr>
+          <td align="center" width="34"><a href="http://twitter.com/djgrill/" target="_blank"><div class="logoTwitter"></div></a></td>
+          <td align="center" width="34"><a href="http://linkedin.com/in/djgrill/" target="_blank"><div class="logoLinkedIn"></div></a></td>
+        </tr>
+      </table>
+    </div>
+  </div>
+
+  <script type="text/javascript">
+    (function() {
+      var po = document.createElement("script"); po.type = "text/javascript"; po.async = true;
+      po.src = "https://apis.google.com/js/plusone.js";
+      var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(po, s);
+    })();
+  </script>
+</body>
+
+</html>
