37dc8ff9f2c9d0c178c383becdd1546293b0590e
David Grilli <djgrill@gmail.com>
2012-09-15 11:33:49 +0200

    Put all English content in a JSON file #29
    modified:   css/misc.css
    modified:   index.php
    modified:   js/script.js
    new file:   lan/en.json

--- index.php
+++ index.php
@@ -48,38 +48,28 @@
 
         <div id="navmenu" class="nav-collapse">
           <ul class="nav">
-            <li class="active about"><a onclick="showAbout()" data-toggle="collapse" data-target=".nav-collapse"><span class="underline">A</span>bout</a></li>
-            <li><a class="external" href="http://blog.djgrill.com/" target="_blank" data-toggle="collapse" data-target=".nav-collapse"><span class="underline">B</span>its<span class="icon visible-desktop"></span><span class="icon hidden-desktop"></span></a></li>
-            <li class="contact"><a onclick="showContact()" data-toggle="collapse" data-target=".nav-collapse"><span class="underline">C</span>ontact</a></li>
-            <li class="developer"><a onclick="showDeveloper()" data-toggle="collapse" data-target=".nav-collapse"><span class="underline">D</span>eveloper</a></li>
-            <li class="experience"><a onclick="showExperience()" data-toggle="collapse" data-target=".nav-collapse"><span class="underline">E</span>xperience</a></li>
-            <li><a class="external" href="http://facebook.com/djGrill/" target="_blank" data-toggle="collapse" data-target=".nav-collapse"><span class="underline">F</span>acebook<span class="icon visible-desktop"></span><span class="icon hidden-desktop"></span></a></li>
-            <li><a class="external" href="https://github.com/djGrill/" target="_blank" data-toggle="collapse" data-target=".nav-collapse"><span class="underline">G</span>itHub<span class="icon visible-desktop"></span><span class="icon hidden-desktop"></span></a></li>
+            <li class="active about"><a onclick="showAbout()" data-toggle="collapse" data-target=".nav-collapse"><span id="nav1"></span></a></li>
+            <li><a class="external" href="http://blog.djgrill.com/" target="_blank" data-toggle="collapse" data-target=".nav-collapse"><span id="nav2"></span><span class="icon visible-desktop"></span><span class="icon hidden-desktop"></span></a></li>
+            <li class="contact"><a onclick="showContact()" data-toggle="collapse" data-target=".nav-collapse"><span id="nav3"></span></a></li>
+            <li class="developer"><a onclick="showDeveloper()" data-toggle="collapse" data-target=".nav-collapse"><span id="nav4"></span></a></li>
+            <li class="experience"><a onclick="showExperience()" data-toggle="collapse" data-target=".nav-collapse"><span id="nav5"></span></a></li>
+            <li><a class="external" href="http://facebook.com/djGrill/" target="_blank" data-toggle="collapse" data-target=".nav-collapse"><span id="nav6"></span><span class="icon visible-desktop"></span><span class="icon hidden-desktop"></span></a></li>
+            <li><a class="external" href="https://github.com/djGrill/" target="_blank" data-toggle="collapse" data-target=".nav-collapse"><span id="nav7"></span><span class="icon visible-desktop"></span><span class="icon hidden-desktop"></span></a></li>
           </ul>
+
+          <div id="lan" class="pull-right">
+            <span id="lan-en" class="active">en</span>
+            <span>/</span>
+            <span id="lan-es">es</span>
+          </div>
         </div>
       </div>
     </div>
   </div>
 
-  <div id="lan" style="display: none">
-    <span id="lanEN" class="selected">en</span>
-    <span>/</span>
-    <span id="lanES">es</span>
-  </div>
-
   <div id="about" class="row-fluid">
     <div class="span1" style="min-height: 0px; height: 0px;">&nbsp;</div>
-    <div class="span6">
-      <p><strong>David Grilli</strong> is a Software Developer from Ciudad Guayana, Venezuela; currently living in Enschede, Netherlands, Europe, Earth, Milky Way, Universe, Multiverse. He's passionate about technology, user experience and web development; and loves learning and doing.</p>
-      <br />
-      <p>He studied Computer Engineering at <a href="http://www.ucab.edu.ve/" target="_blank">UCAB</a> and coursed the CCNA career certification at <a href="http://cisco.netacad.net/" target="_blank">Cisco Networking Academy</a>. He was also the manager of <a href="http://guayana.gtugs.org/" target="_blank">Guayana GDG</a>, a community for people interested in Google's developer technology.</p>
-      <br />
-      <p>When in High School, David showed a notable interest in Basic Sciences; and discovered his passion for <a href="http://www.codecademy.com" target="_blank">Programming</a> during his first year at the University.</p>
-      <br />
-      <p>He enjoys hacking, watching movies, listening to Rock &amp; Roll, running, swimming, throwing frisbee, playing videogames and hanging out with his family and friends.</p>
-      <br />
-      <p class="quote"><i>"I believe in technology<br />bringing people together"</i><br /><span>- David Grilli</span></p>
-    </div>
+    <div id="text1" class="span6"></div>
 
     <div class="social-buttons span5">
       <a href="https://twitter.com/djGrill" class="twitter-follow-button" data-show-count="false" data-lang="en"></a>
@@ -93,91 +83,30 @@
 
   <div id="contact" class="row-fluid">
     <div class="span1" style="min-height: 0px; height: 0px;">&nbsp;</div>
-    <div class="span6">
-      <p>You can contact David anytime to:</p>
-      <ul>
-        <li>Say hi.</li>
-        <li>Talk about a job vacancy.</li>
-        <li>Ask him to join or mentor your startup.</li>
-        <li>Invite him to speak at your event or write for your publication.</li>
-        <li>Give him feedback about his website.</li>
-      </ul>
-      <p>Contact info:</p>
-      <ul class="no-bullets with-icons">
-        <li><span class="logo gmail"></span><strong>Gmail:</strong> djgrill [at] gmail [dot] com</li>
-        <li><span class="logo skype"></span><strong>Skype:</strong> davidgrillidiaz</li>
-      </ul>
-    </div>
+    <div id="text2" class="span6"></div>
 
-    <div class="span5">
-      <p>You can also find him on the following Social Networks:</p>
-      <ul class="no-bullets with-icons">
-        <li><span class="logo twitter"></span><a href="http://twitter.com/djGrill/" target="_blank">Twitter</a></li>
-        <li><span class="logo facebook"></span><a href="http://facebook.com/djGrill/" target="_blank">Facebook</a></li>
-        <li><span class="logo linkedin"></span><a href="http://linkedin.com/in/djgrill/" target="_blank">LinkedIn</a></li>
-        <li><span class="logo foursquare"></span><a href="http://foursquare.com/djgrill/" target="_blank">Foursquare</a></li>
-        <li><span class="logo miso"></span><a href="http://gomiso.com/u/djgrill/" target="_blank">Miso</a></li>
-        <li><span class="logo googleplus"></span><a href="http://profiles.google.com/djgrill/about/" target="_blank">Google+</a></li>
-        <li><span class="logo youtube"></span><a href="http://youtube.com/djgrill/" target="_blank">YouTube</a></li>
-        <li><span class="logo formspring"></span><a href="http://formspring.me/djgrill/" target="_blank">Formspring</a></li>
-      </ul>
-    </div>
+    <div id="text3" class="span5"></div>
   </div>
 
   <div id="developer" class="row-fluid">
     <div class="span1" style="min-height: 0px; height: 0px;">&nbsp;</div>
-    <div class="span6">
-      <p>David has 5+ years of Software Development experience, and has worked with the following technologies:</p>
-      <ul>
-        <li>C/C++</li>
-        <li>Java</li>
-        <li>JavaScript, jQuery</li>
-        <li>Ruby + Rails + Sinatra</li>
-        <li>Python + Django</li>
-        <li>PHP + Zend</li>
-        <li>HTML, HAML</li>
-        <li>CSS, SASS</li>
-        <li>XML, DTD, XSD</li>
-        <li>Mobile: Android, PhoneGap</li>
-        <li>DBMS: MySQL, PostgreSQL, Oracle</li>
-        <li>OS: GNU/Linux, Mac OS, MS Windows</li>
-        <li>Git</li>
-        <li>Heroku</li>
-        <li>Apache HTTP</li>
-        <li>Apache Solr</li>
-      </ul>
-    </div>
+    <div id="text4" class="span6"></div>
 
-    <div class="span5">
-      <p>He is a very motivated developer, with strong logical and programming skills.</p>
-      <p>If you want him to join your team, check the <a onclick="showContact()">Contact</a> section.</p>
-    </div>
+    <div id="text5" class="span5"></div>
   </div>
 
   <div id="experience" class="row-fluid">
     <div class="span1" style="min-height: 0px; height: 0px;">&nbsp;</div>
-    <div class="span6">
-      <p>David has worked as:</p>
-      <ul>
-        <li>Freelance Software Developer.</li>
-        <li>Freelance Professor of Programming, Calculus and Physics.</li>
-        <li>Web Developer at <a href="http://bakedweb.net/" target="_blank">Bakedweb</a>.</li>
-        <li>Software Developer at <a href="http://4geeks.co/" target="_blank">4Geeks</a>.</li>
-        <li>Web Developer at <a href="http://www.ucab.edu.ve/" target="_blank">UCAB</a>.</li>
-      </ul>
-    </div>
+    <div id="text6" class="span6"></div>
 
-    <div class="span5">
-      <p>He's currently working as a <a href="http://en.wikipedia.org/wiki/Web_developer/" target="_blank">Web Developer</a> at an exciting Holland-based startup company.</p>
-      <p>If you think David could match a job vacancy at your company in the future, check the <a onclick="showContact()">Contact</a> section.</p>
-    </div>
+    <div id="text7" class="span5"></div>
   </div>
 
   <div class="navbar navbar-fixed-bottom footer">
     <div class="lnsep"></div>
 
     <div class="row-fluid">
-      <div class="span12">This page is <a href="https://github.com/djGrill/djgrill.com/" target="_blank">Open Source</a> - Last updated Sep 09, 2012</div>
+      <div id="text8" class="span12"></div>
     </div>
 
     <div class="row-fluid">
