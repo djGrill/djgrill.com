fb28875c0a8c7c287c3c22eba1af9bf3649724e5
David Grilli <djgrill@gmail.com>
2013-03-11 20:50:14 -0700

    Fixes separation of social buttons on Firefox
    modified:   index.php

--- index.php
+++ index.php
@@ -84,9 +84,9 @@
     <div class="social-buttons span5">
       <a href="https://twitter.com/djGrill" class="twitter-follow-button" data-show-count="false" data-lang="en"></a>
       <script type="text/javascript">!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-      <span></span>
+      <span>&nbsp;</span>
       <iframe src="https://www.facebook.com/plugins/like.php?href=http://djgrill.com&send=false&layout=button_count" scrolling="no" frameborder="0" style="border:none; width:73px; height:21px"></iframe>
-      <span></span>
+      <span>&nbsp;</span>
       <g:plusone size="medium"></g:plusone>
     </div>
   </div>
