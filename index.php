a7355536c82b0883971b6b049461f021a875972d
David Grilli <djgrill@gmail.com>
2013-03-22 18:41:09 -0700

    Adds verification code for Pinterest. Closes #62
    modified:   index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
   <meta name="robots" content="NOODP,NOYDIR" />
   <meta itemprop="image" content="http://djgrill.com/img/me.jpg" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="p:domain_verify" content="f24fcc34ebf17928d613db50a2b9974a" >
   <link rel="stylesheet" href="css/bootstrap.min.css" />
   <link rel="stylesheet" href="css/misc.css" />
   <link rel="stylesheet" href="css/bootstrap-responsive.min.css" />
